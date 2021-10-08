fb89cec6cb5379de0df4b2d4293f5424fb5fe8c0
iwabuchiy <iwabuchiy@intellilink.cp.jp>
2021-10-08 23:02:11 +0900

    create: model, output, predict.py. modify: study.p

---
+++ app/predict.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import fasttext as ft
+
+# modelの設定
+model = ft.load_model("./models/model.bin")
+
+
+# 推論テスト
+print("推論テスト")
+ret = model.predict("Environmental laws may require the Company to incur substantial expenses and may materially reduce the affected property’s value or limit the Company’s ability to use or sell the affected property.")
+print(ret)
+
+# 推論
+with open('./dataset/test.tsv', 'r') as f_in, open('./results/test_output.tsv', 'w') as f_out:
+    for row in f_in:
+        sid, sentence, html_id = row.strip().split('\t')
+        ret = model.predict(sentence)
+        label = ret[0][0][9]
+        f_out.write('{}\t{}\t{}\t{}\n'.format(sid, sentence, html_id, label))
+
+
+# アウトプット整形
+with open('./processed_dataset/test_output.tsv','r') as f_in, open('./results/submission.tsv', 'w') as f_out:
+    for row in f_in:
+        sid, sentence, html_id, label = row.strip().split('\t')
+        f_out.write('{}\t{}\n'.format(sid, label))
--- app/study.py
+++ app/study.py
@@ -1,27 +1,13 @@
+# -*- coding: utf-8 -*-
 import fasttext as ft
 
 
 # 学習用データ整形
-with open('training.tsv', 'r') as f_in, open('train_ft.txt', 'w') as f_out:
+with open('./dataset/training.tsv', 'r') as f_in, open('./processed_dataset/train_ft.txt', 'w') as f_out:
     for row in f_in:
-        text, label = row.strip().split('\t')
-        f_out.write('__label__{} {}\n'.format(label, text))
-
-# テストデータ整形
-with open('test.tsv', 'r') as f_in, open('test_ft.txt', 'w') as f_out:
-    for row in f_in:
-        text, label = row.strip().split('\t')
-        f_out.write('__label__{} {}\n'.format(label, text))
+        sid, sentence, html_id, label = row.strip().split('\t')
+        f_out.write('__label__{} {}\n'.format(label, sentence))
 
 # 学習:モデル作成
-model = ft.train_supervised(input="train_ft.txt")
-model.save_model("model.bin")”
-
-
-# 推論テスト
-ret = model.predict("Environmental laws may require the Company to incur substantial expenses and may materially reduce the affected property’s value or limit the Company’s ability to use or sell the affected property.	Form10k_09")
-print(ret)
-
-
-# 推論テスト
-ret = model.test("test_ft.txt")
+model = ft.train_supervised("./processed_dataset/train_ft.txt", label_prefix='__label__',epoch=1000, loss="hs")
+model.save_model("./models/model.bin")
